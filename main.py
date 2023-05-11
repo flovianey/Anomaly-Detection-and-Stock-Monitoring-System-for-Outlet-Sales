a32f9b4362bdb94541f3d255584c68d64e8ca5d4
flovianey <129797633+flovianey@users.noreply.github.com>
2023-05-11 16:06:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,20 @@ import pandas as pd
 def check_stores(data_path):
     data = pd.read_excel(data_path)
 
-    march_data = data[data['Period'].dt.month == 3]
-    print(march_data.columns)
-
-    def has_stock(row):
-        return 1 if (row['CLOSING STOCK'] > 0) or (row['SALES'] > 0) else 0
-
-    march_data['Has_Stock'] = march_data.apply(has_stock, axis=1)
-
-    march_agg_data = march_data.groupby(['Store Code', 'Store Name'])['Has_Stock'].agg(['sum', 'count']).reset_index()
-    print(march_agg_data.columns)
+    unique_months = data.Period.unique()
+    last_month_data = data[data.Period == unique_months[-1]]
 
     def classify(row):
         return 'Y' if (row['sum'] / row['count']) >= 0.8 else 'N'
 
-    march_agg_data['Class'] = march_agg_data.apply(classify, axis=1)
+    last_month_data['Has_Stock'] = (last_month_data['CLOSING STOCK'] > 0) | (last_month_data['SALES'] > 0)
+    last_month_agg_data = last_month_data.groupby(['Store Code', 'Store Name'])['Has_Stock'].agg(['sum', 'count']).reset_index()
+    last_month_agg_data['Class'] = last_month_agg_data.apply(classify, axis=1)
 
-    return march_agg_data[march_agg_data.Class == 'N'][['Store Code', 'Store Name']]
+    return last_month_agg_data[last_month_agg_data.Class == 'N'][['Store Code', 'Store Name']]
 
 
 if __name__ == "__main__":
-    data_path = "data.xlsx"
+    data_path = "C:\\Users\\Flovianey ELION\\PycharmProjects\\Stock_checker\\data.xlsx"
     stores = check_stores(data_path)
     print(stores)
